f2f06e970144c84ae438b3d42d9cf468302e383e
apuel <alaaapu135@gmail.com>
2025-03-14 21:30:38 +0200

    docker

--- src/main/java/org/example/alaa/dockerapp/HomeController.java
+++ src/main/java/org/example/alaa/dockerapp/HomeController.java
@@ -1,16 +1,25 @@
 package org.example.alaa.dockerapp;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/home")
 public class HomeController {
+
+    @Autowired
+    private Repo repo;
 
 
     @GetMapping
     public String home() {
         return "Docker Running!!";
     }
+
+    @PostMapping("/add")
+    public User saveUser(@RequestBody User user) {
+       return repo.save(user);
+    }
+
+
 }
--- src/main/java/org/example/alaa/dockerapp/User.java
+++ src/main/java/org/example/alaa/dockerapp/User.java
@@ -1,9 +1,13 @@
 package org.example.alaa.dockerapp;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "_user")
+@Getter
+@Setter
 public class User {
 
     @Id
